7f042e30d5deb7cfb1f88688dc8df70ca5c45089
sertony <serkov_anton@mail.ru>
2017-11-14 13:48:30 +0300

    fix for Android

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import Stripe from './Stripe'
 import PaymentCardTextField from './components/PaymentCardTextField'
 
 export default Stripe
-export { PaymentCardTextField }
+// export { PaymentCardTextField }
